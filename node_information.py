c67629d945cf8c6a00d37cae2e58acc07d061d1b
FedericoStazi <stazi.federico@gmail.com>
2021-03-03 23:24:11 +0100

    Implement support for a single custom feature

--- node_information.py
+++ node_information.py
@@ -8,7 +8,7 @@ def normalize(l):
     return (l - numpy.mean(l)) / (numpy.std(l) if numpy.std(l) > 1e-6 else 1)
 
 def get_nodes_degree(graph):
-    return list(graph.degree())
+    return list(graph.degree(graph.nodes()))
 
 def get_nodes_closeness_centrality(graph):
     return list(networkx.closeness_centrality(graph).values())
